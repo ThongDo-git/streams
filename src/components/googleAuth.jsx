90b0199c38b7a280db9b781429668f7ae02c4f43
Thong Do <tdo05824@gmail.com>
2020-01-27 04:59:24 +0700

    first time practice initialize google library

--- src/components/googleAuth.jsx
+++ src/components/googleAuth.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 
 class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "759419104599-gaistae1fj1gqdj8dgt582flf157vsdo.apps.googleusercontent.com",
+        scope: "email"
+      });
+    });
+  }
+
   render() {
     return <div>Google Auth</div>;
   }
