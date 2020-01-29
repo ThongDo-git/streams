24982a2af2961432c7292f5a1d90ba647899feb7
Thong Do <tdo05824@gmail.com>
2020-01-29 23:31:44 +0700

    second practice building renderAuthButton

--- src/components/googleAuth.jsx
+++ src/components/googleAuth.jsx
@@ -1,17 +1,35 @@
 import React, { Component } from "react";
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "759419104599-0cldhiliiaka214q15qm51p7sdps7fif.apps.googleusercontent.com",
-        scope: "email"
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "759419104599-0cldhiliiaka214q15qm51p7sdps7fif.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I do not know if we sign in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
