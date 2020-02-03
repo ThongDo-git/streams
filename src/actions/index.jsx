8872bfaa5d6b093cf526fdf40e3150cf8e19e5e7
Thong Do <tdo05824@gmail.com>
2020-02-03 18:39:18 +0700

    first time building createStream action

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -13,6 +13,7 @@ export const signOut = () => {
   };
 };
 
-export const createStreams = formValues => async dispatch => {
-  streams.post("/streams", formValues);
+export const createStream = formValues => async dispatch => {
+  const response = await streams.post("/streams", formValues);
+  dispatch({ type: "CREATE_STREAM", payload: response.data });
 };
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
 import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
