09a9438b3a030556fad683101441c53f2e2f7b02
Thong Do <tdo05824@gmail.com>
2020-02-04 22:49:21 +0700

    first time navigating edit

--- src/components/streams/streamList.jsx
+++ src/components/streams/streamList.jsx
@@ -12,7 +12,9 @@ class StreamList extends Component {
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className="right floated content">
-          <button className="ui button primary">Edit</button>
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
           <button className="ui button negative">Delete</button>
         </div>
       );
@@ -20,18 +22,16 @@ class StreamList extends Component {
   }
 
   renderList() {
-    return this.props.streams.map(stream => {
-      return (
-        <div className="item" key={stream.id}>
-          {this.renderAdmin(stream)}
-          <i className="large middle aligned icon camera" />
-          <div className="content">
-            {stream.title}
-            <div className="description">{stream.description}</div>
-          </div>
+    return this.props.streams.map(stream => (
+      <div className="item" key={stream.id}>
+        {this.renderAdmin(stream)}
+        <i className="large middle aligned icon camera" />
+        <div className="content">
+          {stream.title}
+          <div className="description">{stream.description}</div>
         </div>
-      );
-    });
+      </div>
+    ));
   }
 
   renderCreate() {
