875a6a81a4db6217a21fdf86f445f22542853fcb
Thong Do <tdo05824@gmail.com>
2020-02-04 15:30:08 +0700

    second time createBrowserHistory

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamCreate from "./streams/streamCreate";
 import StreamDelete from "./streams/streamDelete";
 import StreamEdit from "./streams/streamEdit";
 import StreamList from "./streams/streamList";
 import StreamShow from "./streams/streamShow";
 import Header from "./header";
+import createBrowserHistory from "./../history";
 
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={createBrowserHistory}>
         <Header />
         <Route path="/streams/new" component={StreamCreate} />
         <Route path="/streams/delete" component={StreamDelete} />
         <Route path="/streams/edit" component={StreamEdit} />
         <Route path="/streams/show" component={StreamShow} />
         <Route path="/" exact component={StreamList} />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
