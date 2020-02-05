512ba2bace819c3ca539827c1135001b40f1f4b7
Thong Do <tdo05824@gmail.com>
2020-02-05 15:17:58 +0700

    copy from branch practice to master

--- src/components/streams/streamList.jsx
+++ src/components/streams/streamList.jsx
@@ -22,16 +22,18 @@ class StreamList extends Component {
   }
 
   renderList() {
-    return this.props.streams.map(stream => (
-      <div className="item" key={stream.id}>
-        {this.renderAdmin(stream)}
-        <i className="large middle aligned icon camera" />
-        <div className="content">
-          {stream.title}
-          <div className="description">{stream.description}</div>
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
         </div>
-      </div>
-    ));
+      );
+    });
   }
 
   renderCreate() {
